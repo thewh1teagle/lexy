2f58f16c79f4367e8f662f8830d013b0bd2b5384
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-06-10 00:17:11 +0300

    fix: deploy error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import  { useEffect, useState, useRef } from 'react';
 import { Worker, createWorker } from 'tesseract.js';
 import './App.css';
 import { CropperRef, Cropper } from 'react-advanced-cropper';
